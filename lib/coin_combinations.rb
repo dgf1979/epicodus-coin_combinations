ab09025dba5a9950b130317686154ea42fb6b273
Drew & Michael <student@epicodus.com>
2015-04-23 09:50:18 -0700

    adds method code to pass quarters unit test

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,5 +1,19 @@
 class Fixnum
   define_method(:coin_combinations) do
-    
+    cents = self
+    change = []
+    quarters = 0
+
+
+    if cents >= 25
+      until cents < 25
+        quarters += 1
+        cents -= 25
+      end
+      change.push("#{quarters} Quarters")
+    end
+
+    change.join(", ")
+
   end
 end

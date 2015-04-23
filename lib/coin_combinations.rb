a62d0d66e92205d2f3f211e8960b637ae54858ca
Drew & Michael <student@epicodus.com>
2015-04-23 09:59:50 -0700

    adds unit test and method code to test addition of nickels and pennies

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -4,6 +4,8 @@ class Fixnum
     change = []
     quarters = 0
     dimes = 0
+    nickels = 0
+    pennies = 0
 
     if cents >= 25
       until cents < 25
@@ -19,6 +21,22 @@ class Fixnum
         cents -= 10
       end
       change.push("#{dimes} Dimes")
+    end
+
+    if cents >= 5
+      until cents < 5
+        nickels += 1
+        cents -= 5
+      end
+      change.push("#{nickels} Nickels")
+    end
+
+    if cents >= 1
+      until cents < 1
+        pennies += 1
+        cents -= 1
+      end
+      change.push("#{pennies} Pennies")
     end
 
     change.join(", ")
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -12,3 +12,9 @@ describe('Fixnum#coin_combinations') do
     expect((85).coin_combinations()).to(eq("3 Quarters, 1 Dimes"))
   end
 end
+
+describe('Fixnum#coin_combinations') do
+  it('returns the number of a quarter for the given number of cents') do
+    expect((91).coin_combinations()).to(eq("3 Quarters, 1 Dimes, 1 Nickels, 1 Pennies"))
+  end
+end
